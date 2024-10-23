19a636eb314cf21cc601419f576eb6ceba39f9be
ab5380 <ab5380@rit.edu>
2024-10-22 21:13:48 -0400

    Test go program

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt" // fmt is a package that contains functions for printing formatted output and scanning input
+
+func main() {
+	fmt.Println("Hello, World!") // Println is a function that prints a line to the standard output
+}
